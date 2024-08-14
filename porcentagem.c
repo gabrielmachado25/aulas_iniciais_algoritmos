019fb662ec0a3500ed9cf218695785508413e36a
gabrielmachado25 <112399650+gabrielmachado25@users.noreply.github.com>
2024-08-14 09:30:34 -0300

    Update porcentagem.c

--- porcentagem.c
+++ porcentagem.c
@@ -1,14 +1,7 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
-
 #include <stdlib.h>
+#include <conio.h>
 #include <stdio.h>
+
 int n;
 int main()
 {
